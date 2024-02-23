7ec2cbe08e627110cfe1d32acac796262ecfe167
Alexander Kosarev <alexkos1004@gmail.com>
2024-02-23 22:03:01 +0300

    image removed

--- src/components/OrderDetails/OrderDetails.jsx
+++ src/components/OrderDetails/OrderDetails.jsx
@@ -7,7 +7,6 @@ const OrderDetails = () => {
         <div className={style.order}>
             <span className={`${style.orderNumber} text text_type_digits-large`}>034536</span>
             <span className="text text_type_main-medium mt-8">идентификатор заказа</span>
-            {/* <img src={image} height="120px" className="m-15"/> */}
             <div className={`${style.image} m-15`}/>
             <span className="text text_type_main-default mb-2">Ваш заказ начали готовить</span>
             <span className="text text_type_main-default text_color_inactive mb-30">Дождитесь готовности на орбитальной станции</span>
